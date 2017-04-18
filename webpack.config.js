e1098831de058634033dbf5ae2a6076e51919a91
Maurizio Mangione <granzebru@gmail.com>
2017-04-18 15:09:05 +0200

    add babel + plugins

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,32 @@ module.exports = {
   entry: path.join(__dirname, "src", "flip-clock.js"),
   output: {
     filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+
+        include: [
+          path.join(__dirname, 'src')
+        ],
+
+        exclude: [
+          path.join(__dirname, 'node_modules')
+        ],
+
+        loader: 'babel-loader',
+
+        options: {
+          plugins: [
+            "transform-custom-element-classes",
+            "transform-es2015-classes"
+          ],
+          presets: [
+            [ 'es2015', {"modules": false} ]
+          ]
+        }
+      }
+    ]
   }
 };
